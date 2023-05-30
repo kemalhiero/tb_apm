48db4e04f1c3760305b690a03c31cd7c35af5bcf
Kemal muhammad <kemalhiero@gmail.com>
2023-05-30 09:57:09 +0700

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Klasifikasi film berdasarkan genre')
+
+st.write("""
+    ### Ketikkan ulasan film yang anda sukai
+""")
+
+data = pd.read_csv('data/imdb_top_1000.csv')
+st.dataframe(data)
+
+form = st.form(key='my-form')
+ulasan = form.text_area('Ulasan film', '')
+submit = form.form_submit_button('Submit')
