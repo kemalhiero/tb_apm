4d29e5bc84b9d0ee165aeff9929a4db2bce86b04
Kemal muhammad <kemalhiero@gmail.com>
2023-06-04 16:37:11 +0700

    edit pickle, genre yang diprediksi masih 0

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 import streamlit as st
-import numpy as np
-import pandas as pd
-from preprocess import preprocess
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import pickle
 
-# st.title('Klasifikasi film berdasarkan genre')
+# Load model from file
+filename = "pickle/model_svm.sav"
+model = pickle.load(open(filename, "rb"))
 
-# st.write("""
-#     ### Ketikkan ulasan film yang anda sukai
-# """)
+# Load vectorizer from file
+vectorizer = pickle.load(open('pickle/vectorizer.sav', 'rb'))
 
-data = pd.read_csv('data/imdb_top_1000.csv')
-# st.dataframe(data)
+# Create a function to predict the class of new data points
+def predict_class(new_data):
+    new_data = vectorizer.transform(new_data)
+    pred = model.predict(new_data)
+    return pred
 
-# form = st.form(key='my-form')
-# ulasan = form.text_area('Ulasan film', '')
-# submit = form.form_submit_button('Submit')
+def preprocess(data):
+    # Lowercase all text
+    data = data.lower()
+
+    # Remove punctuations and special characters
+    data = re.sub('[^a-zA-Z0-9\s]', '', data)
+
+    stop_words = set(stopwords.words('english'))
+    stemmer = PorterStemmer()
+    data = ' '.join([stemmer.stem(word) for word in data.split() if word not in stop_words])
+
+    return data
+
+# Create a Streamlit app
+def main():
+    st.title('Klasifikasi film berdasarkan genre')
+    
+    # Create input field for plot
+    plot = st.text_area('Enter the plot of the film:' )
+
+    # Predict the genre of the film
+    if st.button('Submit'):
+        user_input = preprocess(plot)
+        pred = predict_class([user_input])
+        st.write(f'The predicted genre is {pred[0]}')
+
+
+if __name__ == '__main__':
+    main()
